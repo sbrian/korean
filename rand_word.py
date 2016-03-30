8bbe170050179016f8b3e51d7d6f2fdcdd9f842d
Brian <sbs@hushmail.com>
2016-03-30 19:02:08 +0900

    Ability to sort the words in better ways.

--- rand_word.py
+++ rand_word.py
@@ -1,17 +1,21 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # coding=utf-8
 import re
 import binascii
 import random
 import sys
 import getopt
+import codecs
 
 class AppError(Exception):
+
   pass
 
 class EnglishKoreanDict:
+
   def __init__(self):
-    f = open("dict.txt", "r")
+    #f = open("dict.txt", "r")
+    f = codecs.open('dict.txt', encoding='utf-8-sig')
     self.e_to_k = {} 
     self.k_to_e = {} 
     for line in f:
@@ -20,55 +24,79 @@ class EnglishKoreanDict:
         continue
       if line[0] =="#":
         continue
-      m = re.search(r"^(\xef\xbb\xbf)?([\x20-\x7e]+) (.+) ([0-9,]+)\s*$", line)
+      m = re.search(r"^([ -~]+) (.+) ([0-9,]+)\s*$", line)
       if not m:
         raise AppError("Failed to match in: %s" % line)
-      tag_nums = m.group(4).split(",")
+      tag_nums = m.group(3).split(",")
       tag_dict = {}
       for i in range(0,len(tag_nums)):
         tag_dict[int(tag_nums[i])] = 1
-      self.e_to_k[m.group(2)] = (m.group(3), tag_dict);
-      self.k_to_e[m.group(3)] = (m.group(2), tag_dict);
+      self.e_to_k[m.group(1)] = (m.group(2), tag_dict);
+      self.k_to_e[m.group(2)] = (m.group(1), tag_dict);
+
   def random_word(self, words, group):
     while True:
-      w = words.keys()[random.randint(0,len(words.keys())-1)]
+      w = list(words.keys())[random.randint(0,len(words.keys())-1)]
       if group == 0 or words[w][1].get(group):
-        return w
-  def random_word_game(self, words, group):
-    e = self.random_word(words, group)
-    print e
+        yield w
+
+  def all_words_randomized(self, words, group):
+    w = list(words.keys())
+    random.shuffle(w)
+    for ww in w:
+      if group == 0 or words[ww][1].get(group):
+        yield ww
+
+  def all_words_ordered(self, words, group):
+    w = list(words.keys())
+    w.sort()
+    for ww in w:
+      if group == 0 or words[ww][1].get(group):
+        yield ww
+
+  def word_game(self, words, group, word):
+    print(word)
     sys.stdin.readline()
-    print words[e][0]
-    print
-  def random_words_game(self, words, count, group):
-    for x in range(0, count):
-      print "%d)" % x
-      print
-      self.random_word_game(words, group)
-  def random_korean_words_game(self, count, group):
-    self.random_words_game(self.k_to_e, count, group);
-  def random_english_words_game(self, count, group):
-    self.random_words_game(self.e_to_k, count, group);
+    print(words[word][0])
+    print()
+
+  def words_game(self, words, count, group, word_callback):
+    x=0
+    for w in word_callback(words, group):
+      x+=1
+      if x>count:
+        return
+      print("%d)" % x)
+      print()
+      self.word_game(words, group, w)
+
   def run(self, argv):
     try:
-      opts, args = getopt.getopt(argv, "en:g:")
+      opts, args = getopt.getopt(argv, "en:g:o")
     except getopt.GetoptError:
-      print "Args: -n [number]"
+      print("Args: -n [number]")
       return 2
     english = False
     group = 0
     n = 10
+    ordered = False
     for opt, arg in opts:
       if opt == "-n":
-        n = int(arg)
+        count = int(arg)
       elif opt == "-e":
         english = True
       elif opt == "-g":
         group = int(arg)
+      elif opt == "-o":
+        ordered = True
+    if ordered:
+      sorter = self.all_words_ordered
+    else: 
+      sorter = self.all_words_randomized
     if english:
-      self.random_english_words_game(n, group)
+      self.words_game(self.e_to_k, count, group, sorter);
     else:
-      self.random_korean_words_game(n, group)
+      self.words_game(self.k_to_e, count, group, sorter);
     
 
 if __name__ == "__main__":
